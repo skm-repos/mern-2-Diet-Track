faee549b8f3ec2097f6ba4b914206cf4b4b086fd
Sumit Kumar Manjhi <sumitmanjhi923@gmail.com>
2025-12-31 20:18:09 +0530

    new Changes

--- Diet_Track/src/Components/Home/Header.jsx
+++ Diet_Track/src/Components/Home/Header.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <nav className="relative bg-gray-800 text-white">
+      <div className="max-w-7xl mx-auto px-6">
+        <div className="flex h-16 items-center justify-between">
+
+          {/* Left: Logo */}
+          <div className="shrink-0 text-xl font-bold">
+            MyLogo
+          </div>
+
+          {/* Middle: Nav Links */}
+          <div className="hidden md:flex space-x-8">
+            <a href="#" className="hover:text-gray-300 transition">New</a>
+            <a href="#" className="hover:text-gray-300 transition">Services</a>
+            <a href="#" className="hover:text-gray-300 transition">Testimonials</a>
+            <a href="#" className="hover:text-gray-300 transition">About Us</a>
+          </div>
+
+          {/* Right: Auth Buttons */}
+          <div className="flex space-x-4">
+            <button className="px-4 py-1 rounded hover:bg-gray-700 transition">
+              Login
+            </button>
+            <button className="px-4 py-1 rounded bg-blue-600 hover:bg-blue-500 transition">
+              Sign Up
+            </button>
+          </div>
+
+        </div>
+      </div>
+    </nav>
   )
 }
 
--- Diet_Track/src/Pages/Home.jsx
+++ Diet_Track/src/Pages/Home.jsx
@@ -7,7 +7,7 @@ import Footer from '../Components/Home/Footer'
 
 function Home() {
   return (
-    <div className='min-h-screen bg-gray-200 flex flex-col'>
+    <div className='min-h-screen flex flex-col'>
       <Header/>
       <Hero/>
       <Services/>
