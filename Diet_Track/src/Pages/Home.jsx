b93db7a7e1f0f6cd31af440b107316500ec31f3f
Sumit Kumar Manjhi <sumitmanjhi923@gmail.com>
2025-12-31 08:56:00 +0530

    Made the home page & its components

--- Diet_Track/src/Pages/Home.jsx
+++ Diet_Track/src/Pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Header from '../Components/Home/Header'
+import Hero from '../Components/Home/Hero'
+import Services from '../Components/Home/Services'
+import Testimonials from '../Components/Home/Testimonials'
+import Footer from '../Components/Home/Footer'
 
 function Home() {
   return (
-    <div>
-      Welcome to Home
+    <div className='min-h-screen bg-gray-200 flex flex-col'>
+      <Header/>
+      <Hero/>
+      <Services/>
+      <Testimonials/>
+      <Footer/>
     </div>
   )
 }
